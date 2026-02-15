e87cb2dd993cb53c1c83a4ba214f16d758776394
Motherbloods <habibskh06@gmail.com>
2026-02-15 16:27:59 +0700

    chore: update service worker revision for index.html

--- notes-fe/dev-dist/sw.js
+++ notes-fe/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pp4k1lak8ug"
+    "revision": "0.ngh6do0rqsk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
