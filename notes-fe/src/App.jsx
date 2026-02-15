970d529e2d0d9e97ba6c59b77ea9a7d03b89cc04
Motherbloods <habibskh06@gmail.com>
2026-02-15 15:22:26 +0700

    feat: integrate AuthContext with ProtectedRoute and PublicRoute in App.jsx

--- notes-fe/src/App.jsx
+++ notes-fe/src/App.jsx
@@ -3,34 +3,47 @@ import MainLayout from "./layout/MainLayout.jsx"
 import routes from "./config/routes.jsx"
 import { NotesProvider } from "./context/NotesContext.jsx"
 import { ThemeProvider } from "./context/ThemeContext.jsx"
+import { AuthProvider } from "./context/AuthContext.jsx"
 import Login from "./pages/Login.jsx"
+import ProtectedRoute from "./components/ProtectedRoute.jsx"
+import PublicRoute from "./components/PublicRoute.jsx"
 
 function App() {
   return (
     <ThemeProvider>
-      <NotesProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route element={<MainLayout />}>
-              {routes.map((route, index) => {
-                return route.redirectTo ? (
-                  <Route
-                    key={index}
-                    path={route.path}
-                    element={<Navigate to={route.redirectTo} />}
-                  />
-                ) : (
-                  <Route
-                    key={index}
-                    path={route.path}
-                    element={route.element} />
-                )
-              })}
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </NotesProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <NotesProvider>
+            <Routes>
+              <Route path="/login" element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              } />
+              <Route element={
+                <ProtectedRoute>
+                  <MainLayout />
+                </ProtectedRoute>
+              }>
+                {routes.map((route, index) => {
+                  return route.redirectTo ? (
+                    <Route
+                      key={index}
+                      path={route.path}
+                      element={<Navigate to={route.redirectTo} />}
+                    />
+                  ) : (
+                    <Route
+                      key={index}
+                      path={route.path}
+                      element={route.element} />
+                  )
+                })}
+              </Route>
+            </Routes>
+          </NotesProvider>
+        </AuthProvider>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
