8957fd8402c7569c9da89741374f4fbe966335ae
Motherbloods <habibskh06@gmail.com>
2026-02-25 00:15:31 +0700

    fix: disable devOptions.enabled in production

--- notes-fe/vite.config.js
+++ notes-fe/vite.config.js
@@ -9,9 +9,6 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
       workbox: {
         cleanupOutdatedCaches: true,
         skipWaiting: true,
