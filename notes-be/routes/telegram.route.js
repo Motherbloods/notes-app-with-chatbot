951e6de6ce9c7b604216dfea152df0c6cb9e110c
Motherbloods <habibskh06@gmail.com>
2026-03-03 16:47:34 +0700

    feat: add secret token validation in webhook controller

--- notes-be/routes/telegram.route.js
+++ notes-be/routes/telegram.route.js
@@ -4,5 +4,6 @@ const { confirmLogin } = require("../controllers/telegram.controller");
 const { authMiddleware } = require("../middleware/auth.middleware.js");
 
 router.post("/confirm-login", confirmLogin);
+router.post("/webhook", handleWebhook);
 
 module.exports = router;
